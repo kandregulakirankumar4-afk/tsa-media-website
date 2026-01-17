87e069018712fc893ddf8e7e3b32b1c20fe00118
kandregulakirankumar4-afk <kandregulakirankumar4@gmail.com>
2026-01-17 14:49:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const cards = document.querySelectorAll('.card');
+
+window.addEventListener('scroll', () => {
+  cards.forEach(card => {
+    const position = card.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (position < screenHeight - 100) {
+      card.style.opacity = 1;
+      card.style.transform = 'translateY(0)';
+    }
+  });
+});
